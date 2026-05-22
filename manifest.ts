8d4721ce02e76a77c52ac52256e693fa0ad6c935
Benny <sigalov@fastmail.com>
2026-05-22 07:52:54 -0400

    added icons and other promisional material

--- manifest.ts
+++ manifest.ts
@@ -4,9 +4,21 @@ export default defineManifest({
     manifest_version: 3,
     name: "ChatBar",
     version: "1.0.0",
-    description: "ChatBar Chrome extension.",
+    description: "Attach screenshots of your current browser tab to ChatGPT from the side panel.",
+    icons: {
+        16: "icons/icon-16.png",
+        32: "icons/icon-32.png",
+        48: "icons/icon-48.png",
+        128: "icons/icon-128.png",
+    },
     action: {
         default_title: "ChatBar",
+        default_icon: {
+            16: "icons/icon-16.png",
+            32: "icons/icon-32.png",
+            48: "icons/icon-48.png",
+            128: "icons/icon-128.png",
+        },
     },
     permissions: ["activeTab", "sidePanel"],
     host_permissions: ["<all_urls>"],
