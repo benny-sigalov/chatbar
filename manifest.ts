3a99e92f7544dea6e37c08a1e8258480a78f3958
Benny <sigalov@fastmail.com>
2026-07-08 11:40:21 -0400

    version bump

--- manifest.ts
+++ manifest.ts
@@ -3,7 +3,7 @@ import { defineManifest } from "@crxjs/vite-plugin";
 export default defineManifest({
     manifest_version: 3,
     name: "ChatBar",
-    version: "1.0.0",
+    version: "1.0.1",
     description:
         "Attach screenshots of your current browser tab to ChatGPT from the side panel.",
     icons: {
